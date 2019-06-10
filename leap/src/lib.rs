5bdfca80948bfbc37a50f385d876880c1939b448
hayashi <yasuhiro.yh.hayashi@nts.ricoh.co.jp>
2019-06-10 15:21:06 +0900

    Fix leap

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn is_leap_year(year: u64) -> bool {
-    return divisible(year, 400) || !divisible(year, 100) && divisible(year, 4);
+    divisible(year, 400) || !divisible(year, 100) && divisible(year, 4)
 }
 
 fn divisible(value: u64, divisor: u64) -> bool {
-    return value % divisor == 0;
+    value % divisor == 0
 }
