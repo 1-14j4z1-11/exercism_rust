0816bee81579a6a9d29a7508547327b4cf0629fd
hayashi <yasuhiro.yh.hayashi@nts.ricoh.co.jp>
2019-06-14 17:07:54 +0900

    update sieve

--- sieve/src/lib.rs
+++ sieve/src/lib.rs
@@ -1,27 +1,18 @@
 use std::iter::Iterator;
 
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
-    let mut is_prime = (0..upper_bound + 1).map(|_| true).collect::<Vec<_>>();
-    is_prime[0] = false;
-    is_prime[1] = false;
+    let mut primes = (2..upper_bound + 1).collect::<Vec<u64>>();
+    let mut n = match primes.first() {
+        Some(&x) => x,
+        None => return vec![],
+    };
 
-    for i in 2..is_prime.len() {
-        if !is_prime[i] {
-            continue;
-        }
+    loop {
+        primes.retain(|&x| x == n || x % n != 0);
 
-        for j in (i + 1)..is_prime.len() {
-            if j % i == 0 {
-                is_prime[j] = false;
-            }
-        }
-    }
-
-    let mut primes: Vec<u64> = Vec::new();
-
-    for i in 1..is_prime.len() {
-        if is_prime[i] {
-            primes.push(i as u64);
+        n = match primes.iter().filter(|&&x| x > n).nth(0) {
+            Some(&x) => x,
+            None => break,
         }
     }
 
