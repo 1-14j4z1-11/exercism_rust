dbbd4247e2d2a36ef554208fd7febfb11e43ad19
hayashi <yasuhiro.yh.hayashi@nts.ricoh.co.jp>
2019-09-10 10:00:16 +0900

    bf interpreter

--- others/brainfuck/src/memory.rs
+++ others/brainfuck/src/memory.rs
@@ -11,31 +11,38 @@ impl Memory {
         }
     }
 
+    #[inline]
     pub fn next(&mut self) {
         self.ptr += 1;
         self.extend_memory();
     }
 
+    #[inline]
     pub fn prev(&mut self) {
         self.ptr -= 1;
     }
 
+    #[inline]
     pub fn increment(&mut self) {
         self.memory[self.ptr] += 1;
     }
 
+    #[inline]
     pub fn decrement(&mut self) {
         self.memory[self.ptr] -= 1;
     }
 
+    #[inline]
     pub fn store(&mut self, value: u8) {
         self.memory[self.ptr] = value;
     }
 
+    #[inline]
     pub fn load(&self) -> u8 {
         self.memory[self.ptr]
     }
 
+    #[inline]
     fn extend_memory(&mut self) {
         for _ in self.memory.len()..=self.ptr {
             self.memory.push(0);
