fe57fa80158b0dbcbd2ac4c9376b0ce5481af4eb
hayashi <yasuhiro.yh.hayashi@nts.ricoh.co.jp>
2019-11-27 10:30:07 +0900

    remove busy loop

--- others/thread-pool/src/lib.rs
+++ others/thread-pool/src/lib.rs
@@ -1,7 +1,7 @@
 use std::collections::VecDeque;
 use std::sync::{Arc, Mutex};
 use std::thread::{self, JoinHandle};
-use std::time::Duration;
+use std::sync::mpsc::{channel, Sender};
 
 type TaskQueue = Arc<Mutex<VecDeque<Option<Box<dyn FnOnce() + Send>>>>>;
 
@@ -11,6 +11,7 @@ pub struct ThreadPool {
     is_active: bool,
     join_handles: Vec<JoinHandle<()>>,
     queue: TaskQueue,
+    notification_senders: Vec<Sender<()>>,
 }
 
 impl ThreadPool {
@@ -25,6 +26,7 @@ impl ThreadPool {
             is_active: false,
             join_handles: vec![],
             queue: Arc::new(Mutex::new(VecDeque::new())),
+            notification_senders: vec![],
         })
     }
 
@@ -36,17 +38,27 @@ impl ThreadPool {
 
         for _ in 0..self.n_threads {
             let queue = self.queue.clone();
+            let (s, r) = channel::<()>();
+
+            self.notification_senders.push(s);
+
             let handle = thread::spawn(move || loop {
                 let item = {
                     let mut queue = queue.lock().unwrap();
                     queue.pop_front()
                 };
+
                 match item {
                     Some(opt_task) => match opt_task {
                         Some(task) => task(),
                         None => break,
                     },
-                    None => thread::sleep(Duration::from_millis(100)),
+                    None => {},
+                }
+
+                match r.recv() {
+                    Ok(_) => {},
+                    Err(_) => break,
                 }
             });
 
@@ -67,6 +79,8 @@ impl ThreadPool {
             for _ in 0..self.n_threads {
                 queue.push_back(None);
             }
+
+            self.awake_all_threads();
         }
 
         while !self.join_handles.is_empty() {
@@ -89,7 +103,14 @@ impl ThreadPool {
 
         let mut queue = self.queue.lock().unwrap();
         queue.push_back(Some(Box::new(task)));
+        self.awake_all_threads();
 
         true
+    }
+
+    fn awake_all_threads(&self) {
+        for s in &self.notification_senders {
+            s.send(()).unwrap();
+        }
     }
 }
